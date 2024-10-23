4fcf8baa101cdcd407ffdcc9859515cad0792bfc
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-23 11:33:44 -0500

    all html implemented and ready for toggling, all js global variables defined

--- clients/script.js
+++ clients/script.js
@@ -1,4 +1,53 @@
-const squeakBtn = document.querySelector('#sendBtn')
-const addImage = document.querySelector('#addimage')
-const squeakContent = document.querySelector('#content')
-const logoutBtn = document.querySelector('#logout')
+//Global variables
+    //sidebar
+        const loggedinPage = document.querySelector('.loggedin')
+            const welcomeUser = document.querySelector('#welcomeUser')
+            const userIcon = document.querySelector('#userImage')
+            const sidebarBtn1 = document.querySelector('#sb1')
+            const sidebarBtn2 = document.querySelector('#sb2')
+            const sidebarBtn3 = document.querySelector('#sb3')
+            const sidebarBtn4 = document.querySelector('#sb4')
+            const logoutBtn = document.querySelector('#logout')
+
+        const loggedoutPage = document.querySelector('.loggedout')
+            const usernameLOInput = document.querySelector('#usernameLO')
+            const passwordLOInput = document.querySelector('#passwordLO')
+            const loginBtn = document.querySelector('#login')
+
+        const newAccountPage = document.querySelector('.newAccount')
+            const usernameCAInput = document.querySelector('#usernameCA')
+            const passwordCAInput = document.querySelector('#passwordCA')
+            const createAccountBtn = document.querySelector('#createAccount')
+
+    //feed
+        const feedTitle = document.querySelector('#feedTitle')
+
+        const newSqueak = document.querySelector('.squeak')
+            const addImage = document.querySelector('#addimage')
+            const squeakBtn = document.querySelector('#sendBtn')
+            const squeakContent = document.querySelector('#content')
+
+        const posts = document.querySelector('.posts')
+
+    //account 
+        const accountInfo = document.querySelector('.accountInfo')
+            const inputIconUrl = document.querySelector('#iconUrl')
+            const inputUsernameText = document.querySelector('#accountUsername')
+            const inputPasswordText = document.querySelector('#accountPassword')
+
+        const accountEditBtn = document.querySelector('#accountEdit')
+
+        const accountForm = document.querySelector('#accountInput')
+            const accountIconUrl = document.querySelector('#iconUrlImg')
+            const accountUsername = document.querySelector('#usernameText')
+            const accountPassword = document.querySelector('#passwordText')
+
+        const accountSaveBtn = document.querySelector('#accountSave')
+
+    //my mischief
+        const myMischief = document.querySelector('.myMischief')
+        const followersList = document.querySelector('#followersList')
+
+    //joined mischief
+        const joinedMischiefs = document.querySelector('.joinedMischiefs')
+        const followingList = document.querySelector('#followingList')
